130081e985fe86cd5cae800c91c03798d1ac2fc3
patri-carrasco <patri.carrasco@gmail.com>
2021-02-12 11:31:05 +0100

    Modified data

--- ratings-counter.py
+++ ratings-counter.py
@@ -5,7 +5,7 @@ conf = SparkConf().setMaster("local").setAppName("RatingsHistogram")
 sc = SparkContext(conf = conf)
 
 # lines = sc.textFile("file:///SparkCourse/ml-100k/u.data")
-lines = sc.textFile("./ml-100k/u.data")
+lines = sc.textFile("./data/ml-100k/u.data")
 ratings = lines.map(lambda x: x.split()[2])
 result = ratings.countByValue()
 
