e3c0ccfa6a8524aaa1ab18b4e5ee080a1ba1351c
patri-carrasco <patri.carrasco@gmail.com>
2021-02-17 17:41:31 +0100

    Add exercise total spent dataframe

---
+++ total-spent-customer-sorted-dataframe.py
@@ -0,0 +1,29 @@
+from pyspark.sql import SparkSession
+from pyspark.sql import functions as func
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType, FloatType
+
+#Create session
+spark = SparkSession.builder.appName("TotalSpentByCustomer").master("local[*]").getOrCreate()
+
+# Create schema when reading customer-orders
+schema = StructType([ StructField("cust_id", IntegerType(), True),
+                      StructField("item_id", IntegerType(), True),
+                     StructField("amount_spent", FloatType(), True)])
+
+# Load up the data into spark dataset
+customerDF = spark.read.schema(schema).csv("./data/customer-orders.csv")
+
+customerDF.printSchema()
+
+
+totalByCustomer = customerDF.groupBy("cust_id").agg(func.round(func.sum("amount_spent"),2).alias('total_spent') )
+
+totalByCustomerSorted = totalByCustomer.sort("total_spent")
+
+totalByCustomerSorted.show(totalByCustomerSorted.count())
+
+totalByCustomerSorted.show(totalByCustomerSorted.count())
+
+spark.stop()
+
+
